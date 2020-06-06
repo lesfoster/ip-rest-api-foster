d92b22f704903e7563a4e3060db8e24d8d9c6e63
lesfoster <lfoster.se.be@gmail.com>
2020-06-06 15:09:39 -0400

    Proved bit sets are reasonably performant for this scale.

---
+++ src/test/java/com/project/challenge/BitSetTest.java
@@ -0,0 +1,132 @@
+package com.project.challenge;
+
+import org.apache.commons.net.util.Base64;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.BitSet;
+import java.util.Date;
+
+public class BitSetTest {
+
+    @Test
+    public void testBitSet64K() throws  Exception {
+        BitSet bs = new BitSet(65_536);
+        bs.set(6_000);
+        bs.set(60_000);
+        bs.set(600);
+        bs.set(60);
+        bs.set(6);
+
+        System.out.println(bs.get(6_000) + " " + bs.get(60_000));
+        System.out.println(bs.get(5_000) + " " + bs.get(50_000));
+        byte[] bytes = bs.toByteArray();
+        System.out.println("Makes a byte array of size " + bytes.length);
+        int nonzCt = 0;
+        for (int i = 0; i < bytes.length; i++) {
+            byte b = bytes[i];
+            if (b != 0) {
+                nonzCt ++;
+                System.out.println("Found non-zero value at " + i);
+            }
+        }
+        Assert.assertNotEquals("No encoded values found.", 0, nonzCt);
+    }
+
+    @Test
+    public void testBitSet256() throws  Exception {
+        BitSet bs = new BitSet(256);
+        bs.set(250);
+        bs.set(25);
+        bs.set(2);
+        bs.set(5);
+        bs.set(1);
+
+        System.out.println(bs.get(250) + " " + bs.get(25));
+        System.out.println(bs.get(200) + " " + bs.get(20));
+        byte[] bytes = bs.toByteArray();
+        System.out.println("Makes a byte array of size " + bytes.length);
+        int nonzCt = 0;
+        for (int i = 0; i < bytes.length; i++) {
+            byte b = bytes[i];
+            if (b != 0) {
+                System.out.println("Found non-zero value at " + i);
+                nonzCt ++;
+            }
+        }
+        Assert.assertNotEquals("No encoded values found.", 0, nonzCt);
+    }
+
+    @Test
+    public void testBitSet1024() throws  Exception {
+        BitSet bs = getKBitSet();
+
+        System.out.println(bs.get(1020) + " " + bs.get(25));
+        System.out.println(bs.get(200) + " " + bs.get(20));
+        byte[] bytes = bs.toByteArray();
+        System.out.println("Makes a byte array of size " + bytes.length);
+        int nonzCt = 0;
+        for (int i = 0; i < bytes.length; i++) {
+            byte b = bytes[i];
+            if (b != 0) {
+                System.out.println("Found non-zero value at " + i);
+                nonzCt ++;
+            }
+        }
+
+        Base64 base64 = new Base64();
+        byte[] encoded = base64.encode(bytes);
+        System.out.println("Encoded is " + new String(encoded));
+
+        byte[] redecoded = base64.decode(new String(encoded));
+        for (int i = 0; i < redecoded.length; i++) {
+            byte b = redecoded[i];
+            if (b != 0) {
+                System.out.println("Found non-zero value at " + i);
+            }
+        }
+        byte[] reencoded = base64.encode(redecoded);
+        Assert.assertEquals("Encode-decode-encode test fails.", new String(reencoded), new String(encoded));
+        System.out.println("Encoded length is " + reencoded.length);
+
+        Assert.assertNotEquals("No encoded values found.", 0, nonzCt);
+    }
+
+    @Test
+    public void speedTrial() {
+
+        long startTime = new Date().getTime();
+        // Note: timings gave 5M at ~14 seconds.
+        int iterations = 200_000;
+        for (int i = 0; i < iterations; i++) {
+            BitSet bs = getKBitSet();
+            byte[] bytes = bs.toByteArray();
+            Base64 base64 = new Base64();
+
+            byte[] encoded = base64.encode(bytes);
+            String encStr = new String(encoded);
+            byte[] decoded = base64.decode(encStr);
+            BitSet bs2 = BitSet.valueOf(decoded);
+            if (! bs2.get(1020)) {
+                Assert.fail("Failed recreation test from decoded bytes.");
+            }
+        }
+        long endTime = new Date().getTime();
+        final double elapsedTime = (float) (endTime - startTime) / 1000.0;
+        System.out.println(String.format("Elapsed time is %f seconds for %d iterations", elapsedTime, iterations));
+        Assert.assertTrue("Long time to convert.", elapsedTime < 1.5);
+    }
+
+    private BitSet getKBitSet() {
+        BitSet bs = new BitSet(1024);
+        bs.set(1020);
+        bs.set(512);
+        bs.set(250);
+        bs.set(25);
+        bs.set(2);
+        bs.set(5);
+        bs.set(1);
+        return bs;
+    }
+
+}
